6153be5d283d5b9c408ff409db46b10cba35b96c
megha <stutimegha2019@gmail.com>
2023-10-11 16:38:27 +0530

    add valid annotaion for validation

--- src/main/java/com/microservice/megha/social/media/application/CustomizedResponseEntityCustomExceptions.java
+++ src/main/java/com/microservice/megha/social/media/application/CustomizedResponseEntityCustomExceptions.java
@@ -1,7 +1,10 @@
 package com.microservice.megha.social.media.application;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -29,5 +32,19 @@ public class CustomizedResponseEntityCustomExceptions extends ResponseEntityExce
         //TODO why false in discriptions
 
         return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+//        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now() ,
+//                ex.getFieldError().getDefaultMessage(), request.getDescription(false));
+
+//        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now() ,
+//                ex.getMessage(), request.getDescription(false));
+
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now() ,
+                "Total errors: " +  ex.getErrorCount() + " First error:" + ex.getFieldError().getDefaultMessage() , request.getDescription(false));
+
+        return new ResponseEntity(errorDetails,HttpStatus.BAD_REQUEST);
     }
  }
