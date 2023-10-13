b621b4873552fa9c6dd4d02ab7bb5d04f2410048
megha <stutimegha2019@gmail.com>
2023-10-13 14:58:58 +0530

    add internationalization properties

--- src/main/java/com/microservice/megha/helloworld/HelloWorldController.java
+++ src/main/java/com/microservice/megha/helloworld/HelloWorldController.java
@@ -1,12 +1,22 @@
 package com.microservice.megha.helloworld;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 //@RequestMapping("/hello")
 public class HelloWorldController {
+
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource){
+        this.messageSource =messageSource;
+    }
 
     @GetMapping("/hi")
     public String helloWorld(){
@@ -24,4 +34,11 @@ public class HelloWorldController {
     public HelloWorldBean helloWorldName (@PathVariable String name){
         return new HelloWorldBean("Hello World " + name );
     }
+
+    @GetMapping( path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(){
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message" , null,"Default Message",locale);
+    }
+
 }
